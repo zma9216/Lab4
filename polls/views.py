0695a4128df392f8b9f1b2aa8caa5850c19c227a
zma9216 <zm1@ualberta.ca>
2021-10-01 14:04:34 -0400

    Question #7: Making views render model data

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
